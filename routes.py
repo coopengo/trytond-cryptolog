54d01802afb3edf4c508bd08ea27614e46b05bac
Jérémy Mousset <jeremy.mousset@coopengo.com>
2019-09-18 12:31:11 +0200

    use alias for database

--- routes.py
+++ routes.py
@@ -1,11 +1,12 @@
 from trytond.wsgi import app
-from trytond.protocols.wrappers import with_pool, with_transaction
+from trytond.protocols.wrappers import \
+    with_pool_by_alias, with_transaction
 
 
-@app.route('/cryptolog/<database_name>/callback',
+@app.route('/cryptolog/<database_alias>/callback',
         methods=['GET'])
-@with_pool
-@with_transaction()
+@with_pool_by_alias
+@with_transaction(readonly=False)
 def callback(request, pool):
     Signature = pool.get('document.signature')
     Signature.cryptolog_call_back(request.args)
