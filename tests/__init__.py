1f0d7350aef9ae949c71044570416924420b0ff7
Vincent Garde <vincent.garde@coopengo.com>
2022-10-14 11:12:14 +0200

    merge_6_4

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,2 @@
 # This file is part of Coog. The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-
-try:
-    from trytond.modules.cryptolog.tests.test_cryptolog import suite
-except ImportError:
-    from .test_cryptolog import suite
-
-__all__ = ['suite']
